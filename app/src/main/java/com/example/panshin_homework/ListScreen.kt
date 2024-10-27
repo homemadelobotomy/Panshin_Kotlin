ad8ee45b841ccc70f05dbc91eedd2d4acfc8031b
Maxim <panshinmv@gmail.com>
2024-10-27 16:25:11 +0300

    ДЗ 1

--- app/src/main/java/com/example/panshin_homework/ListScreen.kt
+++ app/src/main/java/com/example/panshin_homework/ListScreen.kt
@@ -3,6 +3,9 @@ package com.example.panshin_homework
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+
+
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,64 +14,57 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun ListScreen() {
+fun ListScreen(dataList: List<String>, addItem: () -> Unit) {
 
-    val dataList = remember { mutableStateListOf<String>() }
-    val itemCount = rememberSaveable { mutableIntStateOf(0) }
     val currentOrientationLandscape =
         (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE)
-    if (dataList.isEmpty()) {
-        for (i in 1..itemCount.intValue) {
-            dataList.add("$i")
-        }
-    }
-    Box(modifier = Modifier.fillMaxSize()) {
+    Column (modifier = Modifier.fillMaxSize()) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(if (currentOrientationLandscape) 4 else 3),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
 
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(if (currentOrientationLandscape) 4 else 3),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .align(Alignment.TopCenter)
-
-        ) {
-            items(dataList) { item ->
-                ListItem(item)
+            ) {
+                items(dataList) { item ->
+                    ListItem(item)
+                }
             }
-        }
 
-        Button(
-            onClick = {
-                itemCount.intValue += 1
-                dataList.add("${itemCount.intValue}")
-            },
-            modifier = Modifier
-                .padding(10.dp)
-                .align(Alignment.BottomCenter)
 
-        ) {
-            Text(text = "Добавить элемент")
+            FloatingActionButton (
+                onClick = addItem,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.CenterHorizontally),
+                containerColor = colorResource(R.color.light_purple)
+            ) {
+                Icon(Icons.Filled.Add, "Добавить")
+            }
+
         }
     }
-}
+
 
 
 fun Modifier.conditional(condition: Boolean, modifier: Modifier.() -> Modifier) : Modifier {
@@ -96,9 +92,7 @@ fun ListItem(number: String) {
     ) {
         Text(
             text = number,
-            color = Color.White ,
-            modifier = Modifier,
-            style = MaterialTheme.typography.bodyMedium,
+            color = Color.White,
             fontSize = 32.sp
         )
     }
--- app/src/main/java/com/example/panshin_homework/MainActivity.kt
+++ app/src/main/java/com/example/panshin_homework/MainActivity.kt
@@ -4,14 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.mutableStateListOf
 
 class MainActivity : ComponentActivity() {
+    private var listSize = 0
+    private val dataList  = mutableStateListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        if (savedInstanceState != null){
+            listSize = savedInstanceState.getInt("key_size")
+            for(i in 1..listSize){
+                dataList.add("$i")
+            }
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ListScreen()
+
+            ListScreen(dataList, addItem = {addItem()})
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("key_size",dataList.size)
+    }
+
+    private fun addItem(){
+        listSize++
+        dataList.add("$listSize")
     }
 }
 
